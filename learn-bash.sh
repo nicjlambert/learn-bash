2e38a18467ade3427664a5d1fe04d42dbe4bcddb
nicjlambert <lambni85@outlook.com>
2022-07-05 23:44:11 +1000

    added variables

--- learn-bash.sh
+++ learn-bash.sh
@@ -1,7 +1,13 @@
- #!/bin/bash
+#!/bin/bash
 
-echo "What is your name?"
+# a positional parameters e.g. ./learn-bash.sh Nicholas Eyes
+name=$1
+compliment=$2
+user=$(whoami)
+date=$(date)
+pwd=$(pwd)
 
-read name
-echo "Good morning $name!"
+echo "Good morning $user! Nice $compliment"
+sleep 1
+echo "The date is $date"
 
