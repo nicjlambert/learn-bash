325d1ee85350e309365c8d713deb34aee90356df
nicjlambert <lambni85@outlook.com>
2022-10-03 11:59:10 +1100

    added  and

--- working-examples/curl.sh
+++ working-examples/curl.sh
@@ -1,7 +1,20 @@
-for i in $(seq 1 50)
+# Description: Downloading data from IRENA API
+# Author: @nicjlambert
+# Date: 2021-09-01
+# Version: 1.0
+# Usage: ./curl.sh
+# Notes:
+# 1. The API is limited to 100,000 values per call
+
+# ASSIGN THE CURRENT YEAR TO A VARIBALE CALLED Date 
+
+YEAR=2022
+CYCLE=2
+
+for i in $(seq 20 50)
 do
 echo "Downloading file: "$i
-curl --fail --location --request POST "https://pxweb.irena.org:443/api/v1/en/IRENASTAT/Power%20Capacity%20and%20Generation/RECAP_2022_cycle2.px" \
+curl --fail --location --request POST https://pxweb.irena.org:443/api/v1/en/IRENASTAT/Power%20Capacity%20and%20Generation/RECAP_"$YEAR"_cycle"$CYCLE".px \
 --header "Content-Type: application/json" \
 --data-raw '{
     "query": [
